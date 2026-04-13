36385518ee80e98781add3e202ef8af5c67daccd
Christopher Carrillo <carrillocrespo17@gmail.com>
2026-04-13 17:09:52 +0200

    Cleaning

--- core/src/main/java/com/christophercarrillo/animationbasic/Main.java
+++ core/src/main/java/com/christophercarrillo/animationbasic/Main.java
@@ -10,36 +10,29 @@ import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 public class Main extends ApplicationAdapter {
-    // Constantes para las filas y columnas del sprite sheet
     private static final int FRAME_COLS = 6;
     private static final int FRAME_ROWS = 5;
 
-    // Objetos necesarios para la animación
     private SpriteBatch batch;
     private Texture walkSheet;
     private Animation<TextureRegion> walkAnimation;
 
-    // Variable para el tiempo transcurrido de la animación
     private float stateTime;
 
-    // Posición del personaje
     private float posX, posY;
 
     @Override
     public void create() {
         batch = new SpriteBatch();
 
-        // Cargar el sprite sheet
         walkSheet = new Texture("animation_sheet.png");
 
-        // Dividir el sprite sheet en una matriz 2D de TextureRegions
         TextureRegion[][] tmp = TextureRegion.split(
             walkSheet,
             walkSheet.getWidth() / FRAME_COLS,
             walkSheet.getHeight() / FRAME_ROWS
         );
 
-        // Convertir la matriz 2D en un array 1D para la animación
         TextureRegion[] walkFrames = new TextureRegion[FRAME_COLS * FRAME_ROWS];
         int index = 0;
         for (int i = 0; i < FRAME_ROWS; i++) {
@@ -48,10 +41,8 @@ public class Main extends ApplicationAdapter {
             }
         }
 
-        // Crear la animación con 0.025 segundos por frame
         walkAnimation = new Animation<>(0.025f, walkFrames);
 
-        // Inicializar variables
         stateTime = 0f;
         posX = 50;
         posY = 50;
@@ -59,16 +50,13 @@ public class Main extends ApplicationAdapter {
 
     @Override
     public void render() {
-        // Limpiar la pantalla
+
         ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
 
-        // Acumular el tiempo transcurrido
         stateTime += Gdx.graphics.getDeltaTime();
 
-        // Obtener el frame actual de la animación
         TextureRegion currentFrame = walkAnimation.getKeyFrame(stateTime, true);
 
-        // Dibujar el frame
         batch.begin();
         batch.draw(currentFrame, posX, posY);
         batch.end();
